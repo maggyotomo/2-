146e9518c547b16b7244a3e20f952b4a95f16279
Tomoya Nakamura <maggyertom@gmail.com>
2017-11-24 18:33:25 +0900

    [add]モック追加

---
+++ dice/Assets/Scripts/DiceValue.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceValue : MonoBehaviour {
+
+    public int value;
+    private Die_d6 die;
+    private Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+        die = gameObject.GetComponent<Die_d6>();
+        rigid = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        value = die.value;
+		
+	}
+    void FixedUpdate()
+    {
+        if (rigid.IsSleeping())
+        {
+            //if (value == 0) Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ dice/Assets/Scripts/RollDice.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollDice : MonoBehaviour {
+    public GameObject dice;
+    public Transform releasePos;
+    public float speed = 100;
+    public int totalValue = 0;
+    private int diceValue = 0;
+    private List<GameObject> diceObjs = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+        diceObjs.AddRange(GameObject.FindGameObjectsWithTag("Dice"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            RollDie();
+        }
+        totalValue = 0;
+        foreach(GameObject diceObj in diceObjs)
+        {
+            diceValue = diceObj.GetComponent<DiceValue>().value;
+            totalValue += diceValue;
+            
+        }
+        Debug.Log(totalValue);
+
+		
+	}
+
+    void RollDie()
+    {
+        GameObject die = GameObject.Instantiate(dice) as GameObject;
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 viewPos = Camera.main.ScreenToViewportPoint(mousePos);
+        Vector3 force = new Vector3((viewPos.x - 0.5f) * speed, (viewPos.y - 0.5f)*speed, speed);
+        //Debug.Log(force);
+        die.GetComponent<Rigidbody>().AddForce(force);
+        die.transform.position = releasePos.position;
+        die.transform.rotation = Random.rotation;
+        diceObjs.Add(die);
+    }
+}
