286c800baa16140667b4be824fb0bcb5099fd2f9
TOMOYA-PC\User <maggyertom@gmail.com>
2017-12-01 00:33:03 +0900

    [add]ダイスいっぱい追加

---
+++ dice/Assets/Scripts/DiceD6_456.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceD6_456 : DiceValue {
+
+    public override int GetDieValue()
+    {
+        int defValue = die.value;
+        switch (defValue)
+        {
+            case 1: return 6;
+            case 2: return 5;
+            case 3: return 4;
+            default: return defValue;
+        }
+    }
+}
--- dice/Assets/Scripts/DiceValue.cs
+++ dice/Assets/Scripts/DiceValue.cs
@@ -5,26 +5,40 @@ using UnityEngine;
 public class DiceValue : MonoBehaviour {
 
     public int value;
-    private Die_d6 die;
-    private Rigidbody rigid;
+    public bool stoped;
+    protected Die die;
+    protected Rigidbody rigid;
 
 	// Use this for initialization
 	void Start () {
-        die = gameObject.GetComponent<Die_d6>();
+        die = gameObject.GetComponent<Die>();
         rigid = gameObject.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        value = die.value;
+        value = GetDieValue();
+        if(gameObject.transform.position.y <= -5)
+        {
+            value = 0;
+            stoped = true;
+        }
 		
 	}
+
     void FixedUpdate()
     {
-        if (rigid.IsSleeping())
-        {
-            //if (value == 0) Destroy(gameObject);
-        }
+        stoped = rigid.IsSleeping();
         
     }
+
+    public virtual int GetDieValue()
+    {
+        return  die.value;
+    }
+    public virtual int GetMaxValue()
+    {
+        return 6;
+    }
+
 }
